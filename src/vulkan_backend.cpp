8511bafdb8f30adcd2537c2330436b36fdbba168
JCM <jacama2009@gmail.com>
2021-04-06 13:50:57 +0200

    Better dependency management

--- src/vulkan_backend.cpp
+++ src/vulkan_backend.cpp
@@ -1,5 +1,4 @@
 #include "vulkan_backend.h"
-#include "vulkan_utils.h"
 
 #include <array>
 #include <bits/stdint-uintn.h>
@@ -15,6 +14,22 @@
 
 namespace vulkanbot
 {
+	uint32_t findMemoryType(vk::PhysicalDeviceMemoryProperties const & memoryProperties, int32_t typeBits, vk::MemoryPropertyFlags requirementsMask)
+	{
+		uint32_t typeIndex = uint32_t( ~0 );
+		for ( uint32_t i = 0; i < memoryProperties.memoryTypeCount; i++ )
+		{
+			if((typeBits & 1) && ((memoryProperties.memoryTypes[i].propertyFlags & requirementsMask) == requirementsMask))
+			{
+				typeIndex = i;
+				break;
+			}
+			typeBits >>= 1;
+		}
+		assert( typeIndex != uint32_t( ~0 ) );
+		return typeIndex;
+	}
+
 	ImageData::ImageData(	vk::PhysicalDevice const & physicalDevice,
 							vk::UniqueDevice const & device,
 				  			vk::Format format_, 
@@ -32,7 +47,7 @@ namespace vulkanbot
 
 		vk::MemoryRequirements memoryRequirements = device->getImageMemoryRequirements(image.get());
 
-		uint32_t memoryTypeIndex = vk::su::findMemoryType(physicalDevice.getMemoryProperties(),
+		uint32_t memoryTypeIndex = findMemoryType(physicalDevice.getMemoryProperties(),
 			memoryRequirements.memoryTypeBits, memoryProperties);
 
 		deviceMemory = device->allocateMemoryUnique(vk::MemoryAllocateInfo(memoryRequirements.size, memoryTypeIndex));
@@ -101,7 +116,7 @@ namespace vulkanbot
 			m_inputImageMemoryRequirements = m_device->getBufferMemoryRequirements(m_inputImageBuffer.get());
 			std::cout << "Input Buffer memory: " << m_inputImageMemoryRequirements.size << std::endl;
 
-			uint32_t memoryTypeIndex = vk::su::findMemoryType(m_physicalDevice.getMemoryProperties(),
+			uint32_t memoryTypeIndex = findMemoryType(m_physicalDevice.getMemoryProperties(),
 				m_inputImageMemoryRequirements.memoryTypeBits,
 				vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent | vk::MemoryPropertyFlagBits::eHostCached);
 			m_inputImageMemory = m_device->allocateMemoryUnique(vk::MemoryAllocateInfo(m_inputImageMemoryRequirements.size, memoryTypeIndex));
@@ -114,7 +129,7 @@ namespace vulkanbot
 			m_outputImageMemoryRequirements = m_device->getBufferMemoryRequirements(m_outputImageBuffer.get());
 			std::cout << "Output Buffer memory: " << m_outputImageMemoryRequirements.size << std::endl;
 
-			uint32_t memoryTypeIndex = vk::su::findMemoryType(m_physicalDevice.getMemoryProperties(),
+			uint32_t memoryTypeIndex = findMemoryType(m_physicalDevice.getMemoryProperties(),
 				m_outputImageMemoryRequirements.memoryTypeBits,
 				vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent | vk::MemoryPropertyFlagBits::eHostCached);
 			m_outputImageMemory = m_device->allocateMemoryUnique(vk::MemoryAllocateInfo(m_outputImageMemoryRequirements.size, memoryTypeIndex));
@@ -205,8 +220,8 @@ namespace vulkanbot
 		vk::PipelineShaderStageCreateInfo fragmentShaderInfo({}, vk::ShaderStageFlagBits::eFragment, fragmentShader.get(), "main");
 		std::array<vk::PipelineShaderStageCreateInfo, 2> shaderStages = { vertexShaderInfo, fragmentShaderInfo };
 
-		auto bindingDescription = Vertex::getBindingDescription();
-		auto attributeDescriptions = Vertex::getAttributeDescriptions();
+		//auto bindingDescription = Vertex::getBindingDescription();
+		//auto attributeDescriptions = Vertex::getAttributeDescriptions();
 
 		vk::PipelineVertexInputStateCreateInfo vertexInputInfo({},
 			0, nullptr, //0, &bindingDescription, 
@@ -244,6 +259,116 @@ namespace vulkanbot
     	}
 
 		return pipeline;
+	}
+
+	TBuiltInResource InitResources()
+	{
+    	TBuiltInResource Resources;
+
+    	Resources.maxLights                                 = 32;
+    	Resources.maxClipPlanes                             = 6;
+    	Resources.maxTextureUnits                           = 32;
+    	Resources.maxTextureCoords                          = 32;
+    	Resources.maxVertexAttribs                          = 64;
+    	Resources.maxVertexUniformComponents                = 4096;
+    	Resources.maxVaryingFloats                          = 64;
+    	Resources.maxVertexTextureImageUnits                = 32;
+    	Resources.maxCombinedTextureImageUnits              = 80;
+    	Resources.maxTextureImageUnits                      = 32;
+    	Resources.maxFragmentUniformComponents              = 4096;
+    	Resources.maxDrawBuffers                            = 32;
+    	Resources.maxVertexUniformVectors                   = 128;
+    	Resources.maxVaryingVectors                         = 8;
+    	Resources.maxFragmentUniformVectors                 = 16;
+    	Resources.maxVertexOutputVectors                    = 16;
+    	Resources.maxFragmentInputVectors                   = 15;
+    	Resources.minProgramTexelOffset                     = -8;
+    	Resources.maxProgramTexelOffset                     = 7;
+    	Resources.maxClipDistances                          = 8;
+    	Resources.maxComputeWorkGroupCountX                 = 65535;
+    	Resources.maxComputeWorkGroupCountY                 = 65535;
+    	Resources.maxComputeWorkGroupCountZ                 = 65535;
+    	Resources.maxComputeWorkGroupSizeX                  = 1024;
+    	Resources.maxComputeWorkGroupSizeY                  = 1024;
+    	Resources.maxComputeWorkGroupSizeZ                  = 64;
+    	Resources.maxComputeUniformComponents               = 1024;
+    	Resources.maxComputeTextureImageUnits               = 16;
+    	Resources.maxComputeImageUniforms                   = 8;
+    	Resources.maxComputeAtomicCounters                  = 8;
+    	Resources.maxComputeAtomicCounterBuffers            = 1;
+    	Resources.maxVaryingComponents                      = 60;
+	    Resources.maxVertexOutputComponents                 = 64;
+    	Resources.maxGeometryInputComponents                = 64;
+    	Resources.maxGeometryOutputComponents               = 128;
+    	Resources.maxFragmentInputComponents                = 128;
+    	Resources.maxImageUnits                             = 8;
+    	Resources.maxCombinedImageUnitsAndFragmentOutputs   = 8;
+    	Resources.maxCombinedShaderOutputResources          = 8;
+    	Resources.maxImageSamples                           = 0;
+    	Resources.maxVertexImageUniforms                    = 0;
+    	Resources.maxTessControlImageUniforms               = 0;
+    	Resources.maxTessEvaluationImageUniforms            = 0;
+    	Resources.maxGeometryImageUniforms                  = 0;
+    	Resources.maxFragmentImageUniforms                  = 8;
+    	Resources.maxCombinedImageUniforms                  = 8;
+    	Resources.maxGeometryTextureImageUnits              = 16;
+    	Resources.maxGeometryOutputVertices                 = 256;
+    	Resources.maxGeometryTotalOutputComponents          = 1024;
+    	Resources.maxGeometryUniformComponents              = 1024;
+    	Resources.maxGeometryVaryingComponents              = 64;
+    	Resources.maxTessControlInputComponents             = 128;
+    	Resources.maxTessControlOutputComponents            = 128;
+    	Resources.maxTessControlTextureImageUnits           = 16;
+    	Resources.maxTessControlUniformComponents           = 1024;
+    	Resources.maxTessControlTotalOutputComponents       = 4096;
+    	Resources.maxTessEvaluationInputComponents          = 128;
+    	Resources.maxTessEvaluationOutputComponents         = 128;
+    	Resources.maxTessEvaluationTextureImageUnits        = 16;
+    	Resources.maxTessEvaluationUniformComponents        = 1024;
+    	Resources.maxTessPatchComponents                    = 120;
+    	Resources.maxPatchVertices                          = 32;
+    	Resources.maxTessGenLevel                           = 64;
+    	Resources.maxViewports                              = 16;
+    	Resources.maxVertexAtomicCounters                   = 0;
+    	Resources.maxTessControlAtomicCounters              = 0;
+    	Resources.maxTessEvaluationAtomicCounters           = 0;
+    	Resources.maxGeometryAtomicCounters                 = 0;
+    	Resources.maxFragmentAtomicCounters                 = 8;
+    	Resources.maxCombinedAtomicCounters                 = 8;
+    	Resources.maxAtomicCounterBindings                  = 1;
+    	Resources.maxVertexAtomicCounterBuffers             = 0;
+    	Resources.maxTessControlAtomicCounterBuffers        = 0;
+    	Resources.maxTessEvaluationAtomicCounterBuffers     = 0;
+    	Resources.maxGeometryAtomicCounterBuffers           = 0;
+    	Resources.maxFragmentAtomicCounterBuffers           = 1;
+    	Resources.maxCombinedAtomicCounterBuffers           = 1;
+    	Resources.maxAtomicCounterBufferSize                = 16384;
+    	Resources.maxTransformFeedbackBuffers               = 4;
+    	Resources.maxTransformFeedbackInterleavedComponents = 64;
+    	Resources.maxCullDistances                          = 8;
+    	Resources.maxCombinedClipAndCullDistances           = 8;
+    	Resources.maxSamples                                = 4;
+    	Resources.maxMeshOutputVerticesNV                   = 256;
+    	Resources.maxMeshOutputPrimitivesNV                 = 512;
+    	Resources.maxMeshWorkGroupSizeX_NV                  = 32;
+    	Resources.maxMeshWorkGroupSizeY_NV                  = 1;
+    	Resources.maxMeshWorkGroupSizeZ_NV                  = 1;
+    	Resources.maxTaskWorkGroupSizeX_NV                  = 32;
+    	Resources.maxTaskWorkGroupSizeY_NV                  = 1;
+    	Resources.maxTaskWorkGroupSizeZ_NV                  = 1;
+    	Resources.maxMeshViewCountNV                        = 4;
+
+    	Resources.limits.nonInductiveForLoops                 = 1;
+    	Resources.limits.whileLoops                           = 1;
+    	Resources.limits.doWhileLoops                         = 1;
+    	Resources.limits.generalUniformIndexing               = 1;
+    	Resources.limits.generalAttributeMatrixVectorIndexing = 1;
+    	Resources.limits.generalVaryingIndexing               = 1;
+    	Resources.limits.generalSamplerIndexing               = 1;
+    	Resources.limits.generalVariableIndexing              = 1;
+    	Resources.limits.generalConstantMatrixVectorIndexing  = 1;
+
+    	return Resources;
 	}
 
 	std::tuple<bool, std::string> VulkanBackend::uploadShader(std::string glslCode)
